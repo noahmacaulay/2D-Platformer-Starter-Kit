0dd6e8412fd6caff8290a300c1a90fc467b823c0
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-01-27 14:07:18 -0400

    Emit jokes in UI

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 signal shakecam(trauma)
 signal troll_laugh
+signal show_joke(text)
 
 var score : int = 0
 
@@ -11,6 +12,7 @@ var score : int = 0
 func add_score():
 	score += 1
 	emit_signal('troll_laugh')
+	joke()
 
 # Loads next level
 func load_next_level(next_scene : PackedScene):
@@ -18,3 +20,12 @@ func load_next_level(next_scene : PackedScene):
 
 func shake_camera(trauma):
 	emit_signal('shakecam', trauma)
+
+func joke():
+	var jokes = ["Have you heard the one about the sheep? You have?",
+	"When I got up this morning, I had hope!",
+	"Why did the chicken cross the road? Uh... I forget...",
+	"How many engineers does it take to change a lightbulb? Not too many, or they can't engin-hear",
+	"Where does France keep its armies? In its sleevies!"]
+	var n = randi_range(0, len(jokes) - 1)
+	emit_signal("show_joke", jokes[n])
--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -3,6 +3,17 @@ extends Control
 @onready var score_texture = %Score/ScoreTexture
 @onready var score_label = %Score/ScoreLabel
 
+@onready var joke_display = $Panel/JokeDisplay
+@onready var animation_player = $Panel/AnimationPlayer
+
+
+func _ready():
+	GameManager.connect("show_joke", self._on_show_joke)
+
 func _process(_delta):
 	# Set the score label text to the score variable in game maanger script
 	score_label.text = "x %d" % GameManager.score
+
+func _on_show_joke(in_text):
+	joke_display.text = in_text
+	animation_player.play("display_joke")
