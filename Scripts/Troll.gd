e52d626f2ad9b918ce2d6ae1d8f7add227757056
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-01-27 12:57:48 -0400

    update troll and scroll sprites

--- Scripts/Troll.gd
+++ Scripts/Troll.gd
@@ -1,12 +1,18 @@
 extends Sprite2D
 
 @onready var animation_player = $AnimationPlayer
+var serious_sprite = preload("res://Assets/Textures/Troll_3.png")
+var laughing_sprite = preload("res://Assets/Textures/Troll_1.png")
 
 func _ready():
 	GameManager.connect("troll_laugh", self._on_laugh)
 	
 func _on_laugh():
+	texture = laughing_sprite
 	animation_player.play("laugh")
 
 func call_shake():
 	GameManager.shake_camera(0.5)
+
+func _on_laugh_end():
+	texture = serious_sprite
