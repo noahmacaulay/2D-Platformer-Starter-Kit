bb9eab4fb662d09abbbd6768579d13bc53843dbe
noahmacaulay <usernameneeded@gmail.com>
2024-01-27 16:29:11 -0400

    move
    slowed move speed

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # --------- VARIABLES ---------- #
 
 @export_category("Player Properties") # You can tweak these changes according to your likings
-@export var move_speed : float = 400
+@export var move_speed : float = 300
 @export var jump_force : float = 600
 @export var gravity : float = 30
 @export var max_jump_count : int = 2
