fe518ca8484cc69b78eee9631eb228ce1cbdf31b
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-01-28 01:17:54 -0400

    Infinite level

---
+++ Scripts/DynamicLevel.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+
+@onready var plat_scene = preload("res://Scenes/Prefabs/Platform.tscn")
+@onready var scroll_scene = preload("res://Scenes/Prefabs/Scroll.tscn")
+
+var time = 0
+var timeDirection = 1
+var moveDuration = 2
+var destination: Vector2 = Vector2.ZERO
+@export var speed = 40
+var num_vertical_platforms = 7
+
+func _ready():
+	generate_platforms(Vector2(-480, -544))
+	generate_platforms(Vector2(-480, -1440))
+	GameManager.connect('troll_laugh', self.move_down)
+
+func _process(delta):
+	position = position.move_toward(destination, delta * speed)
+	if position == destination:
+		readjust()
+
+func generate_platforms(starting_pos : Vector2):
+	randomize()
+	var locations_x = range(6)
+	var locations_y = range(8)
+	locations_x.shuffle()
+	locations_y.shuffle()
+	locations_y.pop_back()
+	var scroll_i = [randi_range(0, len(locations_y))]
+	scroll_i.append(scroll_i[0]+randi_range(1, len(locations_y)) % len(locations_y))
+	for i in range(len(locations_y)):
+		var new_plat : Node2D = plat_scene.instantiate()
+		add_child(new_plat)
+		var pos : Vector2 = starting_pos
+		pos.x += locations_x[i%len(locations_x)] * 192
+		pos.y += locations_y[i] * 128
+		new_plat.position = pos
+		if i in scroll_i:
+			var new_scroll : Node2D = scroll_scene.instantiate()
+			new_plat.add_child(new_scroll)
+			new_scroll.position.y -= 80
+
+func readjust():
+	var nodes = get_children()
+	for n : Node2D in nodes:
+		n.position += position
+		if n.position.y > 895:
+			remove_child(n)
+			n.queue_free()
+	position = Vector2.ZERO
+	destination = Vector2.ZERO
+
+func move_down():
+	destination.y += 448
+	if roundi(destination.y) % 896 == 0:
+		generate_platforms(Vector2(-480, destination.y - 544))
