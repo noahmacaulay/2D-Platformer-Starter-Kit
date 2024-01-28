b3757bfc0c43fdb1a9f9954db0b8b256a4bc7100
noahmacaulay <usernameneeded@gmail.com>
2024-01-28 15:22:38 -0400

    troll laugh
    asdasd

--- Scripts/Troll.gd
+++ Scripts/Troll.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var laugh_player = $LaughPlayer
+@onready var lower_laugh_player = $LowerLaughPlayer
 var serious_sprite = preload("res://Assets/Textures/Troll_3.png")
 var laughing_sprite = preload("res://Assets/Textures/Troll_1.png")
 
@@ -11,8 +13,13 @@ func _on_laugh():
 	texture = laughing_sprite
 	animation_player.play("laugh")
 
+func call_shake_first():
+	GameManager.shake_camera(0.5)
+	laugh_player.play()
+
 func call_shake():
 	GameManager.shake_camera(0.5)
+	lower_laugh_player.play()
 
 func _on_laugh_end():
 	texture = serious_sprite
