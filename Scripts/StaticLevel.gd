808c99f78bddb6a0006a16b67d493d13dc0a3882
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-01-28 10:47:20 -0400

    Add rock particles on laugh

--- Scripts/StaticLevel.gd
+++ Scripts/StaticLevel.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 var boulder_tscn = preload("res://Scenes/Prefabs/Boulder.tscn")
+@onready var rock_particles : CPUParticles2D = $RockParticles
 
 func _ready():
 	GameManager.connect('troll_laugh', self.make_boulders)
-	
+
 func make_boulders():
+	rock_particles.emitting = true
 	for i in range(3):
 		var new_b = boulder_tscn.instantiate()
 		add_child(new_b)
